8e3ce3e420972f2dcd98ed0c8b690dcf88cb13a4
algendeshmukh <sddeshmukh1007@gmail.com>
2022-09-24 06:22:44 +0530

    These are the initial author routes

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ if (process.env.NODE_ENV !=="production") {
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser")
 
 const indexRouter = require("./routes/index")
+const authRouter = require("./routes/authors")
 //setting up the view engine
 app.set("view engine","ejs")
+//using body bodyParser..taken from bodyParser doc!
+app.use(bodyParser.urlencoded({limit:"10mb",extended: false}))
 // views location
 app.set("views",__dirname +"/views")
 //hooking up express layouts
-app.set("layouts","Layouts/layout")
+app.set("layout","layouts/layout")
 //force it to use express layouts
 app.use(expressLayouts)
 //Location of our public files
@@ -28,5 +32,6 @@ db.on("error",error => console.error(error))
 db.once("open", () => console.log("You are connected to mongoose"))
 
 app.use("/",indexRouter)
+app.use("/authors",authRouter)
 
 app.listen (process.env.PORT || 3000)
